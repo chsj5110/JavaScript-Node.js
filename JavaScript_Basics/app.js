3435336988068bbf88c8c0d4dba7f164e9124112
Soojin <chsj5110@gmail.com>
2024-03-24 20:32:33 +0900

    24.03.18_JavaScript Basics Excercise

---
+++ JavaScript_Basics/app.js
@@ -0,0 +1,10 @@
+let greetingText = "Hi, I'm Soojin!";
+let greetingNum = 77;
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job={
+    title: 'Developer',
+    place: 'New York',
+    salary: 50000
+};
+
+let adultYears
