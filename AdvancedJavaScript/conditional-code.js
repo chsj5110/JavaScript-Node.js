24926f704362b001cb27421114d83460af833337
Soojin <chsj5110@gmail.com>
2024-04-18 20:41:33 +0900

    24.04.18 Loop-in-action

---
+++ AdvancedJavaScript/conditional-code.js
@@ -0,0 +1,11 @@
+const myName = 'Soojin';
+
+if(myName === 'Soojin'){
+    console.log('Hello!');
+}
+
+let isLoggedIn = true;
+
+if(isLoggedIn === true) {
+    console.log('User is logged in!');
+}
